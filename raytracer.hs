9267954857973d564d83e67fd02cb9ff2b6377f1
aiken <acairncross@gmail.com>
2015-09-14 23:40:13 +0100

    Add planes

--- raytracer.hs
+++ raytracer.hs
@@ -5,36 +5,33 @@ import Data.Maybe (fromMaybe)
 import Data.List (intercalate, minimumBy)
 import Control.Monad (liftM, liftM2, sequence)
 
+type Radius = Double
 type Point = Vector3
 type Direction = Vector3
-type Radius = Double
-type Sphere = (Point,Radius)
 type PointLight = Point
 type Ray = (Point,Direction)
 type Intersection = (Point,Direction)
 type ReflIntersection = (PointLight,Intersection)
 
+data Surface = Sphere Point Radius | Plane Point Direction
+
 width = 500 :: Int
 height = 500 :: Int
 
-spheres :: [Sphere]
-spheres = [((-25, -25, 380), 30),
-           ((100, 0, 500), 80),
-           ((-50, 150, 300), 50),
-           ((-100, -100, 700), 100),
-           ((-100, -100, 500), 15)]
+surfaces :: [Surface]
+surfaces = [Sphere (-100,0,200) 100, Plane (0,-40,0) (0,1,0)]
 
-α = 3 :: Double
+α = 100 :: Double
 
 lights :: [Point]
-lights = [(-100, 0, 250), (500, 100, 1000)]
+lights = [(200,300,0)]
 
 raytrace :: Ray -> Double
 raytrace (o,l) =
-  let intersection = (intersect (o,l) spheres)
+  let intersection = (intersect (o,l) surfaces)
       reflIntersect light =
         (\(p,n) -> let shadowRay = (p,normalize (light⊖p))
-                       shadowIntersection = intersect shadowRay spheres
+                       shadowIntersection = intersect shadowRay surfaces
                    in  case shadowIntersection of
                               Nothing -> Just (light,(p,n))
                               Just _ -> Nothing)
@@ -45,13 +42,15 @@ raytrace (o,l) =
   in  sum (map ((fromMaybe 0) . (liftM intensity))
                (sequence (map ((=<<) . reflIntersect) lights) intersection))
 
-intersect :: Ray -> [Sphere] -> Maybe Intersection
+intersect :: Ray -> [Surface] -> Maybe Intersection
 intersect (o,l) [] = Nothing
 intersect (o,l) ss =
-  let (c,r) = minimumBy (maybeCompare `on` (intersectSphere (o,l))) ss
+  let s = minimumBy (maybeCompare `on` (intersectSurface (o,l))) ss
       distToIntersection = (\d -> let point = o⊕(d `scale` l)
-                                  in  (point, normalize (point⊖c)))
-  in  fmap distToIntersection (intersectSphere (o,l) (c,r))
+                                  in  case s of
+                                        Sphere c r -> (point, normalize (point⊖c))
+                                        Plane p n -> (point, n))
+  in  fmap distToIntersection (intersectSurface (o,l) s)
 
 -- Nothing is max instead of min
 maybeCompare :: Ord a => Maybe a -> Maybe a -> Ordering
@@ -60,14 +59,18 @@ maybeCompare :: Ord a => Maybe a -> Maybe a -> Ordering
 Nothing `maybeCompare` (Just x) = GT
 Nothing `maybeCompare` Nothing = EQ
 
-intersectSphere :: Ray -> Sphere -> Maybe Double
-intersectSphere (o,l) (c,r)
+intersectSurface :: Ray -> Surface -> Maybe Double
+intersectSurface (o,l) (Sphere c r)
   | discriminant > 0 && dMinus > 0.001 = Just dMinus
   | discriminant > 0 && dPlus > 0.001 = Just dPlus
   | otherwise = Nothing
   where discriminant = (l⋅(o⊖c))**2 - ((o⊖c)⋅(o⊖c)) + r**2
         dMinus = -(l⋅(o⊖c)) - sqrt discriminant
         dPlus = -(l⋅(o⊖c)) + sqrt discriminant
+intersectSurface (o,l) (Plane p n) =
+  let d = ((p⊖l)⋅n)/(l⋅n)
+  in if abs (l⋅n) == 0 || d < 0.001 then Nothing else Just d
+
 
 imageToWorld :: (Int,Int) -> Vector3
 imageToWorld (i,j) = ((fromIntegral j) - (fromIntegral width)/2,
@@ -78,7 +81,7 @@ image :: [[Int]]
 image = let matmax xss = maximum [(maximum xs) | xs <- xss]
             matscale c xss  = [[c*x | x <- xs] | xs <- xss]
             matfloor xss = [[floor x | x <- xs] | xs <- xss]
-            mat = [[ raytrace (imageToWorld (i,j), (0,0,1))
+            mat = [[ raytrace (imageToWorld (i,j), normalize ((imageToWorld (i,j))⊖(0,0,-1000)))
                    | j <- [0..width-1] ]
                    | i <- [0..height-1] ]
         in  matfloor $ matscale (255/(matmax mat)) mat
